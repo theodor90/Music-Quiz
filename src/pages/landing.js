a078a0d937979fcd516c008291f7df9b82ad2eed
David Ruland <davidruland@MacBook-Air-som-tillhor-David.local>
2024-05-13 15:48:42 +0200

    Update buttons and icons for landing page

--- src/pages/landing.js
+++ src/pages/landing.js
@@ -10,21 +10,21 @@ function LandingPage() {
     return (
         <Container>
             <div className="landing-layout">
-                <motion.div
+                <motion.div className=""
                     initial={{ x: '100vw' }} // Initial position of the image (off-screen to the right)
                     animate={{ x: 0 }} // Animate to position 0 (center of the screen)
                     transition={{ type: 'spring', stiffness: 120, duration: 1 }} // Animation type and duration
                     style={{ position: 'fixed', top: 0, right: 0 }}
                 >
-                    <img src={Guitar} className="landing-img" alt="Your Image" />
+                 <img src={Guitar} className="landing-img" alt="Your Image" />
                 </motion.div>
                 <div class="center">
                     <button class="button">Play</button>
                 </div>
 
                 <div class="bottom-buttons">
-                    <button class="button"><img src="left-icon.png" alt="Left Icon" /> Left</button>
-                    <button class="button">Right <img src="right-icon.png" alt="Right Icon" /></button>
+                    <button class="button-left"><i className="fa fa-exclamation"/></button>
+                    <button class="button-right"><i className="fa fa-question"/></button>
                 </div>
             </div>
         </Container>
