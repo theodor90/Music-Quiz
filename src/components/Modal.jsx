c98bead1b1e4fbd4c9f1dce17784887c83649a1c
mattiasvlex <165170151+mattiasvlex@users.noreply.github.com>
2024-05-14 13:40:40 +0200

    Tab format update Modal.jsx

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -4,23 +4,23 @@ import './Modal.css';
 
 
 function Modal() {
-  return (
-    <Popup trigger=
-      {<button>Play</button>} 
-      modal nested>
-      { close => (
-        <div className="modal">
-          <button className="close" onClick={close}>
-            &times;
-          </button>
-          <div className="content">
-            <button>Single Player</button>
-            <button>Multiplayer</button>
-          </div>
-        </div>
-      )}
-    </Popup>
-  )
+    return (
+        <Popup trigger=
+            {<button>Play</button>} 
+            modal nested>
+            { close => (
+                <div className="modal">
+                    <button className="close" onClick={close}>
+                        &times;
+                    </button>
+                    <div className="content">
+                        <button>Single Player</button>
+                        <button>Multiplayer</button>
+                    </div>
+                </div>
+            )}
+        </Popup>
+    )
 }
 
 export default Modal;
