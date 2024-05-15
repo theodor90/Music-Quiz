322c76a59c2c15f53f3f0ee3300f793d8d996b81
al-sw <alanjunior.sweeney@gmail.com>
2024-05-15 15:54:19 +0200

    Updated gameface

--- src/components/GameFace.js
+++ src/components/GameFace.js
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import Database from "../database.json";
+import "./GameFace.css";
 
 const GameFace = () => {
   const [quizQuestions, setQuizQuestions] = useState(Database);
@@ -10,6 +11,10 @@ const GameFace = () => {
 
   const correctAnswer = q1.songName;
 
+// 1. MAKE IT SO THAT THE RIGHT ANSWER TURNS GREEN, OTHERS RED. Add a "Next question" button after an answer has been clicked.
+// 2. SHOW ANSWERS AFTER X AMOUNT OF TIME, BASED ON TIMER
+// 3. RANDOMIZE BUTTON ORDER
+
   function checkAnswer(event) {
     if (event.target.id === correctAnswer) {
       console.log("Right answer!");
@@ -17,23 +22,29 @@ const GameFace = () => {
     } else {
       console.log("Wrong answer!");
     }
+
+    // Takes user to the next question
     const nextQuestionIndex = currentQuestionIndex + 1;
     if (nextQuestionIndex < quizQuestions.length) {
       setCurrentQuestionIndex(nextQuestionIndex);
     } else {
+      // Add "Endgame.jsx" here
       console.log("End of questions!");
     }
   }
 
   return (
-    <div>
+    <div id="gamePage">
       <p>Score: {score}</p>
       <h3>Whats song is this?</h3>
-      {q1.answers.map((answer) => (
-        <button key={answer} id={answer} onClick={checkAnswer}>
+      <div className="game-btns">
+        {q1.answers.map((answer) => (
+        <button key={answer} id={answer} onClick={checkAnswer} className="btn btn-secondary">
           {answer}
         </button>
       ))}
+      </div>
+      
     </div>
   );
 };
