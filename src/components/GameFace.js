f7d23202d003b693951e685d6151c9def80c3204
al-sw <alanjunior.sweeney@gmail.com>
2024-05-15 15:35:30 +0200

    1

---
+++ src/components/GameFace.js
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import Database from "../database.json";
+
+const GameFace = () => {
+  const [quizQuestions, setQuizQuestions] = useState(Database);
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  let [score, setScore] = useState(0);
+
+  const q1 = quizQuestions[currentQuestionIndex];
+
+  const correctAnswer = q1.songName;
+
+  function checkAnswer(event) {
+    if (event.target.id === correctAnswer) {
+      console.log("Right answer!");
+      setScore(prevScore => prevScore + 1);
+    } else {
+      console.log("Wrong answer!");
+    }
+    const nextQuestionIndex = currentQuestionIndex + 1;
+    if (nextQuestionIndex < quizQuestions.length) {
+      setCurrentQuestionIndex(nextQuestionIndex);
+    } else {
+      // Add "Endgame.jsx" here
+      console.log("End of questions!");
+    }
+  }
+
+  return (
+    <div>
+      <p>Score: {score}</p>
+      <h3>Whats song is this?</h3>
+      {q1.answers.map((answer) => (
+        <button key={answer} id={answer} onClick={checkAnswer}>
+          {answer}
+        </button>
+      ))}
+    </div>
+  );
+};
+
+export default GameFace;
