5714ae8d3126edeedfbf104e1f16f4e3eb7d8765
al-sw <alanjunior.sweeney@gmail.com>
2024-05-15 15:27:57 +0200

    updated gameface

--- src/components/GameFace.js
+++ src/components/GameFace.js
@@ -3,20 +3,31 @@ import Database from "../database.json";
 
 const GameFace = () => {
   const [quizQuestions, setQuizQuestions] = useState(Database);
-  const q1 = Database[0];
+  const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+  let [score, setScore] = useState(0);
+
+  const q1 = quizQuestions[currentQuestionIndex];
 
   const correctAnswer = q1.songName;
 
   function checkAnswer(event) {
     if (event.target.id === correctAnswer) {
       console.log("Right answer!");
+      setScore(prevScore => prevScore + 1);
     } else {
       console.log("Wrong answer!");
+    }
+    const nextQuestionIndex = currentQuestionIndex + 1;
+    if (nextQuestionIndex < quizQuestions.length) {
+      setCurrentQuestionIndex(nextQuestionIndex);
+    } else {
+      console.log("End of questions!");
     }
   }
 
   return (
     <div>
+      <p>Score: {score}</p>
       <h3>Whats song is this?</h3>
       {q1.answers.map((answer) => (
         <button key={answer} id={answer} onClick={checkAnswer}>
