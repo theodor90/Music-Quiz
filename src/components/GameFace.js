aca1a6534c6c193b8d73a29c59460ba9de8495c4
al-sw <alanjunior.sweeney@gmail.com>
2024-05-15 15:16:27 +0200

    Updated DB and GameFace

--- src/components/GameFace.js
+++ src/components/GameFace.js
@@ -1,22 +1,30 @@
-import { useState } from 'react';
-import Database from '../database.json';
+import { useState } from "react";
+import Database from "../database.json";
 
 const GameFace = () => {
-  const [quizQuestions, setQuizQuestions] = useState(Database)
-  const q1 = Database[0]
+  const [quizQuestions, setQuizQuestions] = useState(Database);
+  const q1 = Database[0];
+
+  const correctAnswer = q1.songName;
+
+  function checkAnswer(event) {
+    if (event.target.id === correctAnswer) {
+      console.log("Right answer!");
+    } else {
+      console.log("Wrong answer!");
+    }
+  }
+
   return (
-    /* <div>{q1.map(() => (
-       <div key={id}><h3>{songName}
-       </h3><p>Artist: {artist}</p>
-         <iframe src={url} width="100%" height="166" frameBorder="no" allowFullScreen={true}>
-         </iframe></div>))}</div>);*/
     <div>
       <h3>Whats song is this?</h3>
-      {q1.answers.map((answer) =>(
-        <button key={answer}>{answer}</button>
+      {q1.answers.map((answer) => (
+        <button key={answer} id={answer} onClick={checkAnswer}>
+          {answer}
+        </button>
       ))}
     </div>
-  )
+  );
 };
 
 export default GameFace;
