c8d20c1a5c4d1bf5c798ed614cd7449a73714421
David Ruland <davidruland@MacBook-Air-som-tillhor-David.local>
2024-05-16 13:29:58 +0200

    Update React player and database

--- src/components/GameFace.js
+++ src/components/GameFace.js
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ReactPlayer from 'react-player'
 import Database from "../database.json";
 import "./GameFace.css";
 import "../styles/components.css"
@@ -12,9 +13,9 @@ const GameFace = () => {
 
   const correctAnswer = q1.songName;
 
-// 1. MAKE IT SO THAT THE RIGHT ANSWER TURNS GREEN, OTHERS RED. Add a "Next question" button after an answer has been clicked.
-// 2. SHOW ANSWERS AFTER X AMOUNT OF TIME, BASED ON TIMER
-// 3. RANDOMIZE BUTTON ORDER
+  // 1. MAKE IT SO THAT THE RIGHT ANSWER TURNS GREEN, OTHERS RED. Add a "Next question" button after an answer has been clicked.
+  // 2. SHOW ANSWERS AFTER X AMOUNT OF TIME, BASED ON TIMER
+  // 3. RANDOMIZE BUTTON ORDER
 
   function checkAnswer(event) {
     if (event.target.id === correctAnswer) {
@@ -36,17 +37,31 @@ const GameFace = () => {
 
   return (
     <div id="gamePage">
-      <p>Score: {score}</p>
-      <h3>Whats song is this?</h3>
+      <p className="score">Score: {score}</p>
+      <h3 className="song-title">Whats song is this?</h3>
+      <div className='player-wrapper'>
+            <ReactPlayer
+              className='react-player'
+              url={q1.url}
+              pip={false}
+              playing={true}
+              controls={false}
+              light={false}
+              auto_play={true}
+              playbackRate={1.0}
+              volume={0.8}
+              muted={false}
+            />
+          </div>
       <img src={q1.profileImg} className="artist-img"></img>
       <div className="game-btns">
         {q1.answers.map((answer) => (
-        <button key={answer} id={answer} onClick={checkAnswer} className="btn btn-secondary">
-          {answer}
-        </button>
-      ))}
+          <button key={answer} id={answer} onClick={checkAnswer} className="btn btn-secondary">
+            {answer}
+          </button>
+        ))}
       </div>
-      
+
     </div>
   );
 };
